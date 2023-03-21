a10a53cad452850a9009e0bc196875467802f4af
buradabhargavi <110055258+buradabhargavi@users.noreply.github.com>
2023-03-21 11:36:05 +0530

    about_us.php added

---
+++ about_us.php
@@ -0,0 +1,79 @@
+<?php include 'connect.php'?>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Movie Time</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <header >
+        <a class="logo" href="/"><img src="images/log2.jpg" alt="logo" width="150dp" height="150dp"></a>
+        
+        <nav>
+            <h1 style="text-align: center;font-size:50px;">MOVIE TIME</h1>
+            <ul class="nav__links">
+                <li><a href="services.php">Services</a></li>
+                <li><a href="aboutus.php">About</a></li>
+                <li><a href="index.php">Home</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <h2 style="font-family:Montserrat;color: #BF516A;font-size:30px;text-align:center;margin-top:58px;margin-bottom:50px;">ABOUT US</h2>
+    <center><p>This website is solely a work of final year engineering project done for the benefit of finding out the details of a movie.</p></center>
+
+
+
+
+
+
+
+
+    <div class="main-wrap">
+        <div class="footer-wrap">
+            <div class="footer-wrap-1">
+                <div class="footer-section">
+                    <h1>About</h1>
+                    <a href="services.php">Services</a>
+                    <a href="#">Portfolio</a>
+                </div>
+                <div class="footer-section">
+                    <h1>Resources</h1>
+                    <a href="#">Pics</a>
+                    <a href="#">Blog</a>
+                </div>
+                <div class="footer-section">
+                    <h1>Contact</h1>
+                    <a href="#">Help</a>
+                    <a href="#">Advertise</a>
+                </div>
+            </div>
+            <div class="footer-wrap-2">
+                <center>
+                    <div class="social-link">
+                        <a href="#">
+                            <img src="images/fb.png" alt="Facebook">
+                        </a>
+                        <a href="#">
+                            <img src="images/google.png" alt="Google">
+                        </a>
+                        <a href="#">
+                            <img src="images/linkedin.png" alt="linkedin">
+                        </a>
+                        <a href="#">
+                            <img src="images/twitter.png" alt="twitter">
+                        </a>
+                    </div>
+                </center>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
